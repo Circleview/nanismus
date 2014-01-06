dcd6533c6ae021c6d897089a31d244b8b500b3a4
Stefan <swilluda@web.de>
2014-01-06 11:43:37 +0100

    Changes to Issue #3 , iPod like scrollwheel
    Includes an interpreter to get the amount of water that should be send
    to the arduino. The amount of water comes from the ipod.html and ist
    postsend to the water.php and further to the waterbar.php.
    Changes the height of the waterbar to fit the small iframe in the
    watering popup.
    Some changes in the texts.
    Some cleanup.

--- website/src/wasser/waterbar.php
+++ website/src/wasser/waterbar.php
@@ -12,12 +12,20 @@ $delay = $delay * 1000000; // convert into microseconds
 //$zufall = rand(1,3); // Zufällige länge der Aktion simulieren
 $gesamtdauer = 10; // Wie lange in Sekunden soll der Ladebalken angezeigt werden?
 $schrittdauer = 1; // Sekunden  // Dauer zwischen zwei Aktualisierungen
-$wassermenge = 400;  // Wie viel Wasser wird gewässert?
-$maxwassermenge = 400; // Wie viel Wasser kann maximal gegossen werden?
+
+$maxwassermenge = 500; // Wie viel Wasser kann maximal gegossen werden?
+if ($_GET['menge'] == 0)
+{
+    $wassermenge = $maxwassermenge;    
+}
+else
+{
+    $wassermenge = $_GET['menge'];  // Wie viel Wasser wird gewässert?    
+}
  
 $maxfortschritt = 100; // Progressbar Maximalwert = 100%
-$maxhoehesaeule = 245; // Wenn so viel Wasser wie möglich gegossen wird, 
-// dann ist die Wassersäule am höchsten. 
+$maxhoehesaeule = 105; // Wenn so viel Wasser wie möglich gegossen wird, 
+// dann ist die Wassersäule am höchsten. in pixel
 
 $hoehesaeule = $maxhoehesaeule * $wassermenge / $maxwassermenge; // Die Höhe der Wassersäule auf der Website
 
@@ -38,14 +46,23 @@ If ($_GET['status'] == 0) // Beim ersten Aufrufen der Funktion
 // soll der Gießbefehl an das Arduino gesendet werden)
 {
     // Sende den Gießbefehl an das Arduino Board
-    arduino_send($arduino_ip,$arduino_port,"P".chr($menge));
+    // arduino_send($arduino_ip,$arduino_port,"P".chr($menge));
+	if ($wassermenge == 0) {
+		arduino_send($arduino_ip,$arduino_port,"P".chr($menge));
+	}
+	else if ($wassermenge <= 255) {
+		arduino_send($arduino_ip,$arduino_port,"Q".chr($menge));
+	}
+	else {
+		$menge = $wassermenge - 255;
+		arduino_send($arduino_ip,$arduino_port,"R".chr($menge));
+	}    
     // Gießung anordnen
 	// Warte x * 1.000.000 Mikro-Sekunden
     usleep($delay); // to avoid connection refusal  
     
-    // für Testzwecke kann der $returnvalue hier manupuliert werden
-	global $returnvalue;    
-    //$returnvalue = 9; 
+    // für Testzwecke kann der $returnvalue hier manupuliert werden    
+    //$returnvalue = 8; 
 }
 
 if ($returnvalue == 8) // Wenn die Erde jetzt doch noch feucht genug ist, dann
@@ -53,7 +70,7 @@ if ($returnvalue == 8) // Wenn die Erde jetzt doch noch feucht genug ist, dann
 // Das soll dann auf der Website angezeigt werden;
 {
     $php_array['status'] = $maxfortschritt;
-    $php_array['message'] = '<h1>W&auml;sserung doch nicht n&ouml;tig </h1><p><a href="../../index.php">zur&uuml;ck zur Nani</a></p>';
+    $php_array['message'] = '<h1>W&auml;sserung doch nicht n&ouml;tig</h1><p>Die Erde war noch feucht.<a href="../../index.php" target="_top">zur&uuml;ck</a></p>';
 }
 else // Wenn die Erde trocken war, dann
 // hat das Arduino begonnen zu wässern. Das soll auf der Website
@@ -76,7 +93,7 @@ else // Wenn die Erde trocken war, dann
     if($php_array['status'] != $maxfortschritt) {
     	$php_array['message'] = '<h1>bzzzzzz...</h1><p>'.$php_array['status']*$mljeprozentpunkt.' ml von '.$wassermenge.' ml</p>'; //, verbleibend: '.($wassermenge-$php_array['status']*$mlje100);
     } else {
-    	$php_array['message'] = '<h1>W&auml;sserung abgeschlossen</h1><p><a href="../../index.php">zur&uuml;ck zur Nani</a></p>';
+    	$php_array['message'] = '<h1>W&auml;sserung abgeschlossen</h1><p><a href="../../index.php" target="_top">zur&uuml;ck zur Nani</a></p>';
     }   
 }
 // Ausgabe des PHP Arrays als JSON Objekt
@@ -91,158 +108,10 @@ function arduino_send($ip,$port,$command) {
     $res = @fsockopen($ip,$port);    // @ schaltet fehlermeldungen aus
     if($res) {
         fwrite($res,$command);
+        global $returnvalue;
         $returnvalue = fread($res,2);
 
         //echo "Returnvalue: $returnvalue <br />" ;
     }
 }
-?>
-
-<?php
-
-	       /*
-
-            if(isset($_POST['waessern']))
-            {
-
-                // Wenn eine manuelle Bewässerung angeordnet wurde, dann soll diese
-                // in die Log Tabelle geschrieben werden
-
-                // Diese Funktion wird zunächst nicht in Betrieb genommen, da das
-                // Arduino die Gießmenge in die Datenbank schreibt. Die Gießmenge
-                // Wird zusammen mit dem Timestamp in der Tabelle erfasst, sodass ein
-                // zusätzlicher Logtype keinen Vorteil zu bringen scheint.
-
-                //Datenbank-Verbindung herstellen
-                //--------------------------------
-                /*
-                 include("db.php");
-                 //include("db_test.php");
-
-                        $table = "plant_log" ;
-                        //$table = "plant_log_test" ;
-                        // !! WICHTIG !!
-                        // der logtype ist für ein Gießevent noch zu definieren, für das
-                        // Gießen gibt es bisher keinen logtype
-
-                        // http://stackoverflow.com/questions/1995562/now-function-in-php
-                        $timestamp = date("Y-m-d H:i:s");
-                        $sql = "
-                          INSERT INTO $table
-                          (
-                          sensorname , logtype , value , timestamp
-                          )
-                          VALUES
-                          (
-                          '$name', '$type', $value, '$timestamp'
-                          )
-                        ";
-                        $db_erg = mysqli_query($db_link, $sql)
-                            or die("<p>Anfrage fehlgeschlagen</p>" . mysqli_error($db_link));
-
-                            echo "Dateneingabe erfolgreich";
-                        }
-                        else
-                        {
-                            echo "Dateneingabe fehlerhaft";
-                        }
-                */
-
-                // Sende den Gießbefehl an das Arduino Board
-                //arduino_send($arduino_ip,$arduino_port,"P".chr($menge));
-                // Gießung anordnen
-    			// Warte x * 1.000.000 Mikro-Sekunden
-                //usleep($delay); // to avoid connection refusal
-
-            /*} /*else
-            {
-                //arduino_send($arduino_ip,$arduino_port,"M".chr(0));
-                arduino_send($arduino_ip,$arduino_port,"M".chr(0)); // Gibt den aktuellen Feuchtigkeitswert als Status aus
-				// Warte x * 1.000.000 Mikro-Sekunden
-                usleep($delay); // to avoid connection refusal
-            }
-
-            if ($debug)
-            {
-                echo "Prozentfeuchte 0: $prozentfeuchte[0] %<br>";
-                echo "Prozentfeuchte 1: $prozentfeuchte[1] %<br>";
-                echo "Prozentfeuchte 1: $prozentfeuchte[2] %<br>";
-                echo "Returnvalue 0: $returnvalue[0] <br>";
-                echo "Returnvalue 1: $returnvalue[1] <br>";
-                echo "Returnvalue 1: $returnvalue[2] <br>";
-            }
-            */
-?>
-
-<?php     
-
-//header('Content-Type: text/html; charset=utf-8');
-
-/* Problem mit utf-8 Konvertierung und Zeichenanzeige im
- * Zusammenhang mit einer MYSQL Datenbank, konnte durch den
- * HTML Tag weiter unten nicht abgefangen werden
- * http://www.winfuture-forum.de/index.php?showtopic=193063
- * brachte die Lösung mit dem PHP Header Tag.
- */
-
-//session_start();
-
-// Funktionen
-
-/*$arduino_ip="nanismus.no-ip.org";
-//$arduino_ip="192.168.178.30";
-$arduino_port="24";
-
-$debug = 0;             // throws additional information if true
-$test = 1;               // grab different data from different databases due to the testcontext
-
-global $delay;
-       $delay = 0.3; // delay in seconds
-                 // http://de1.php.net/manual/de/function.usleep.php
-       $delay = $delay * 1000000; // convert into microseconds
-
-	/*
-	DUMMY
-
-	$Werte = array(555, 666);
-	echo "$Werte[0], ";
-	echo "$Werte[1], ";
-
-	// http://xhtmlforum.de/49701-php-die-ersten-3-zeichen-eines.html
-	// $text = "einstring";
-	// echo "$text, ";
-	$text_ohne_ersten_3_zeichen = substr("$Werte[1]", 0, 1);
-	echo "$text_ohne_ersten_3_zeichen, ";
-
-	// http://phpforum.de/forum/showthread.php?t=145722
-	$stringMitLeerInDerMitte = "text1;text2;text3";
-
-	list($wort1,$wort2)=explode(";",$stringMitLeerInDerMitte);
-	echo "$wort1, $wort2";
-	*/
-
-/*function arduino_send($ip,$port,$command) {
-
-    $res = @fsockopen($ip,$port);    // @ schaltet fehlermeldungen aus
-    if($res) {
-        fwrite($res,$command);
-
-		global $returnvalue;
-        $returnvalue = fread($res,2);
-
-        //echo "Returnvalue: $returnvalue <br />" ;
-    }
-}
-
-/*
- * Arduino States für Feuchtigkeit und Wohlbefinden der Pflanze
- * // state
-    #define WATER_OVERFLOW 5               // There is water on the ground of the pot
-    #define NO_WATER_OVERFLOW 4            // There is no Water on the ground
-    #define URGENT_SENT 3                  // Soil is critically dry
-    #define SOON_DRY_SENT 2                // Soil is not moist anymore
-    #define MOISTURE_OK 1                  // Soil is moist
-    #define TOOWET 0                       // Soil is too wet
-*/
-
 ?>
