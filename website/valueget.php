740438b7fd42e4c96158fce960013a295c71d0d2
Stefan <swilluda@web.de>
2014-04-11 23:46:29 +0200

    fix #37

--- website/valueget.php
+++ website/valueget.php
@@ -1,5 +1,7 @@
 <?php
-// Autor: Jens Leopold 
+
+// Idee von
+// Autor: Jens Leopold
 //
 // Kontakt: info@jleopold.de
 //
@@ -40,17 +42,16 @@ include("src/db.php");
         }
 
         // Erhaltene Daten des Arduino Http Get in die MySQL Datenbank speichern
-                
+
         // http://nanismus.no-ip.org/nanismus_test/valueget.php?name=Banane&type=status&value=99&key=c3781633f1fb1ddca77c9038d4994345
              
-        if ((isset($_GET['name'])) and (isset($_GET['type'])) and (isset($_GET['value'])) and (($_GET['key']) == $key)) 
+        if ((isset($_GET['name'])) and (isset($_GET['type'])) and (isset($_GET['value'])) and (($_GET['key']) == $key))
         {	// Wenn 'TEMP' übergeben wurde und key stimmt...
-        	$name = ($_GET['name']);
+
+            $name = ($_GET['name']);
         	$type = ($_GET['type']);
             $value = ($_GET['value']);
-        	//echo $value;
-        	//$eintragen = mysql_query("INSERT INTO plantmeasures_test (value,timestamp)	VALUES ($value, NOW())");	// TEMP real übergeben, DATE = automatischer SQL-Befehl (NOW)
-        
+
         // http://stackoverflow.com/questions/1995562/now-function-in-php
         $timestamp = date("Y-m-d H:i:s");
         $sql = "
@@ -63,14 +64,26 @@ include("src/db.php");
           '$name', '$type', $value, '$timestamp'
           )
         ";
+
+        //http://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol
         $db_erg = mysqli_query($db_link, $sql) 
             or die("<p>Anfrage fehlgeschlagen</p>" . mysqli_error($db_link));
-            
-            echo "Dateneingabe erfolgreich <br>";
+
+        ?>
+            <!DOCTYPE html>
+                <html>
+                    <body>
+
+                        <?php echo "<p>Dateneingabe erfolgreich</p>"; ?>
+
+                    </body>
+                </html>
+        <?php  
         }
         else
         {
             echo "Dateneingabe fehlerhaft <br>";
+            //echo "0 \r\n";            
         }
         
         if ($type == "Nachricht")
@@ -107,11 +120,10 @@ include("src/db.php");
             if ($messageID == 2 || $messageID == 3 || $messageID == 6 || $messageID == 13 || $messageID == 49)
             {
                 $user = $twitteruser;
-                $url = ''; // Zur Zeit blockiert twitter Nachrichten mit der nanismus website
-                // mal gucken, ob ich die Adresse irgendwann wieder verwenden kann. 
+                $url = 'http://nanismus.de'; // war mal eine Zeit blockiert
+                // mal gucken, ob ich die Adresse jetzt wieder verwenden kann.
                 // bis dahin muss die URL leider draußen bleiben.
                 // Den Twitter Support habe ich am 26.01.2014 angeschrieben und um Entsperrung gebeten
-                //$url = "http://nanismus.de";
             }
             else
             {
@@ -121,79 +133,60 @@ include("src/db.php");
             
             // Bei Nachrichten, die sich auf eine Wässerung beziehen sollen die Wässerungsmenge 
             // im Tweet enthalten 
-            // Hier kann man die die Wassermenge manipulieren, um der Filterung von 
+            // Hier kann man die die Wassermenge manipulieren, um der Filterung von
             // doppelten Tweets vorzubeugen
-            //$menge = 350;
+            // $menge = 350;
 
             // Wenn die MsgID eine Nachricht enthält, die sagt, dass gewässert wurde
             // dann soll die Gießmenge noch mit ausgegeben und der eigentlichen Nachricht angefügt werden
             if ($messageID == 4  || $messageID == 6 ||
             $messageID == 47 || $messageID == 49 || $messageID == 68 || $messageID == 69)
             {
-            
-                // Wie lauteten die letzten Nachricht, die die Banane gesendet hat?
-                // Um das zu ermitteln, müssen wir zuerst die Nachrichten-ID der letzten Nachricht ermitteln
-                // Wann hat die Banane das letzte Mal eine Nachricht verschickt?
-                $msgcount = 1; // Anzahl der Nachrichten aus der Vergangenheit anzeigen
-                // 100 und 99 werden als Nachrichten ausgeklammert, da diese keine echten Nachrichten sind
-                $sql = "
-                SELECT $tabelle.value, $tabelle.timestamp
-                FROM $tabelle
-                WHERE (($tabelle.sensorname = 'Banane') AND ($tabelle.logtype = 'Nachricht') AND ($tabelle.value != 100) AND ($tabelle.value != 99))
-                ORDER BY $tabelle.ID DESC LIMIT $msgcount
-                ";
-        
-                $db_erg = mysqli_query( $db_link, $sql );
-                if ( ! $db_erg )
-                {
-                    die('Ungültige Abfrage: ' . mysqli_error($db_link));
-                }
-        
-                while($row = mysqli_fetch_array($db_erg, MYSQL_ASSOC))
-                {
-                    //$msgid = $row['value'];
-                    $msgtime = $row['timestamp'];
-                    //echo "msgtime: ". $msgtime."<br />";
-                    //echo "msgid: ". $msgid."<br />";
-                }
-            
-                $sql = "
-                SELECT $tabelle.value
-                FROM $tabelle
-                WHERE ((($tabelle.sensorname)='Banane') AND (($tabelle.logtype)='Giessmenge') AND (($tabelle.timestamp) <= '$msgtime'))
-                ORDER BY $tabelle.ID DESC LIMIT 1
-                ";
+              // Frage die letzte Gießmenge in der Datenbank ab
+              $sql = "
+              SELECT $tabelle.value
+              FROM $tabelle
+              WHERE ((($tabelle.sensorname)='Banane') AND (($tabelle.logtype)='Giessmenge'))
+              ORDER BY $tabelle.ID DESC LIMIT 1
+              ";
 
-                $db_erg = mysqli_query( $db_link, $sql );
-                if ( ! $db_erg )
-                {
-                    die('Ungültige Abfrage: ' . mysqli_error($db_link));
-                }
+              $db_erg = mysqli_query( $db_link, $sql );
+              if ( ! $db_erg )
+              {
+                  die('Ungültige Abfrage: ' . mysqli_error($db_link));
+              }
 
-                while($row = mysqli_fetch_array($db_erg, MYSQL_ASSOC))
+              while($row = mysqli_fetch_array($db_erg, MYSQL_ASSOC))
+              {
+                  $menge = $row['value'];
+                  //echo "aktuelle Menge: ". $menge."<br /><br />";
+              }
+
+              // Für den unwahrscheinlichen Fall, dass gar kein Wert in der Datenbank steht
+              // Nimm einfach die normale Nachricht ohne Zusatzinformationen
+              if (!empty($menge))
+              {
+                // Entscheide anhand der MessageID welchen Nachrichtenzusatz die
+                // Twitternachricht erhalten soll.
+                switch ($messageID)
                 {
-                    $menge = $row['value'];
-                    //echo "aktuelle Menge: ". $menge."<br /><br />";
+                    case 6:
+                    case 49:
+                        $message = $message." ".$menge." ml reichten nicht.";
+                        break;
+                    case 68:
+                    case 69:
+                        $message = $message." ".$menge." ml waren zu viel.";
+                        break;
+                    case 4:
+                    case 47:
+                        $message = $message." ".$menge." ml waren genau was ich brauchte.";
+                        break;
+                    default:
+                        break;
                 }
+              }
             }
-            
-            switch ($messageID) 
-            {
-                case 6:
-                case 49: 
-                    $message = $message." ".$menge." ml reichten nicht.";
-                    break;
-                case 68:
-                case 69:
-                    $message = $message." ".$menge." ml waren zu viel.";
-                    break;
-                case 4:
-                case 47:
-                    $message = $message." ".$menge." ml waren genau was ich brauchte.";
-                    break;
-                default: 
-                    break;
-            }                 
 
             echo "Nachricht: ", $message.' '.$user.' '.$url,"<br /><br />";
                       
