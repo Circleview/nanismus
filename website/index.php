863e4282d5c684f588ae36af18bda599a48763c5
Stefan <stefanwilluda@bg-mb-air-8stefan.fritz.box>
2016-08-15 20:10:30 +0200

    related to #71
    * task: add a favicon

--- website/index.php
+++ website/index.php
@@ -13,11 +13,12 @@
 <!-- // iOS Homescreen-Icon
     https://developer.apple.com/library/ios/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html -->
 <!-- https://icomoon.io/app/ // find nice icons fast and free of charge -->
-<!-- http://www.colorpicker.com/a1d490 // tiny colorpicker -->
+<!-- http://www.colorpicker.com/309c0c // tiny colorpicker -->
+<!-- current leaf color 309c0c -->
 <link rel="apple-touch-icon" sizes="120x120" href="/images/leaf.png">
 
 <!-- Favicon -->
-<link href="images/favicon_gruen" type="image/x-icon" rel="shortcut icon" />
+<link href="images/favicon" type="image/x-icon" rel="shortcut icon" />
 
 
 <!-- PHP load data from mySQL database to show on this page -->
