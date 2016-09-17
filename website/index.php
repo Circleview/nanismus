0e59cd126e36d1a437d3c8cd8a2430ef57eed005
Stefan <stefanwilluda@bg-mb-air-8stefan.fritz.box>
2016-09-17 23:02:25 +0200

    fix #85 fix #86 related to #79 * Include the background color choice in a separate php file to call it in production and test to make color changes available in test and production immediately * Include the font color choice in a separate php file to call it in production and test to make the color changes available in test and production immediately * Change the current php calls to php function calls to simply return the value of the function based on input data

--- website/index.php
+++ website/index.php
@@ -3,10 +3,10 @@
 
 <head>
 
-<!-- some kind of header information will re-occur on many pages -->
+<!-- some kind of header information will re-occur on may pages -->
 <?php
- include ("src/page_metadata.php");
-          ?>
+    include ("src/page_metadata.php");
+    ?>
 
 <!-- Define feature toogles -->
 <?php
@@ -17,6 +17,9 @@
     // feature toggle to display the twitter timeline for nanismusKW
     $showTwitterTimeline = true;
     
+    // feature toggle to display the moisture chart
+    $showMoistureChart = true;
+    
     ?>
 
 <!-- PHP load data from mySQL database to show on this page -->
@@ -24,12 +27,13 @@
     
     // on production we don't want to see the test data
     $name = "Banane";
+    
     include ("src/dbabfrage.php");
     
     /* for tests */
-    // $Feuchte = 18;
+    // $Feuchte = 44;
     
-?>
+    ?>
 
 <!-- include aditional data -->
 <!-- text constants which will be displayed as labels -->
@@ -40,89 +44,93 @@
 
 <!-- dynamic styles based on database value -->
 <style>
+
+/* http://www.w3schools.com/colors/colors_picker.asp */
+
+body {
     
-    /* http://www.w3schools.com/colors/colors_picker.asp */
+    /* change the background color based on the current moisture of the plant.
+     * use green, yellow and red */
+background:
     
-    body {
-        
-        /* change the background color based on the current moisture of the plant. 
-         * use green, yellow and red */
-        background:
-
-        <?php
-
-            if ($Feuchte >=41){
-                echo "#82c837;"; /* green */
-            }
-            else if ($Feuchte >=21){
-                echo "#FFFF33;"; /* "#F2EC38;"; /* yellow */
-            }
-            else {
-                echo "#C8373A;"; /* red */
-            }
+    <?php
     
-        ?>
-    }
-
-    td {
-        
-        /* change the td font color based on the current moisture of the plant.
-         * use green, yellow and red */
-        color:
-
-        <?php
-
-        if ($Feuchte >=41){
-            echo "#ffffff;"; /* green */
-        }
-        else if ($Feuchte >=21){
-            echo "#000000;"; /* "#F2EC38;"; /* yellow */
-        }
-        else {
-            echo "#ffffff;"; /* red */
+    include ("src/background_color.php");
     
-        }
+    echo backgroundColor($Feuchte); echo ";"; /* red */
+    
+    ?>
+}
 
-        ?>
-    }
+td {
+    
+    /* change the td font color based on the current moisture of the plant.
+     * use green, yellow and red */
+color:
+    
+    <?php
+    
+    include ("src/text_color.php");
+    
+    echo textColor($Feuchte); echo ";"; /* red */
+    
+    ?>
+    
+}
 
 </style>
+
+<!-- if the feature toggle is swiched on include all the data that is needed to display the moisture diagram -->
+<?php
+    if ($showMoistureChart) {
+        
+        include ("src/moistchart_setup.php");
+        
+    }
+    ?>
 
 </head>
 
 <body>
 
-    <table>
-        <tr>
-            <td>
-                <! -- build up the content of this row and show an icon of a waterdrop and the current moisture value -->
-                <?php
-                    include ("src/moisture_value_display.php");
-                    ?>
-            </td>
-        </tr>
-        <tr>
-            <td class="buttonTableData">
-                <!-- check if we need to include the watering button -->
-                <?php
-                    include ("src/watering_button.php");
-                    ?>
+<table>
+<tr>
+<td>
 
-            </td>
-        </tr>
+<! -- build up the content of this row and show an icon of a waterdrop and the current moisture value -->
+<?php
+    include ("src/moisture_value_display.php");
+    ?>
+</td>
+</tr>
+<tr>
+<td class="buttonTableData">
+<!-- check if we need to include the watering button -->
+<?php
+    include ("src/watering_button.php");
+    ?>
 
-        <! -- build up the table row that displays the last manual watering intiation timestamp - if the feature toggle has been set -->
-            <?php
-                include ("src/last_manual_watering_timestamp.php");
-                ?>
+</td>
+</tr>
 
-        <! -- build up the table row that displays the twitter timeline for nanismusKW -
-            if the feature toggle has been set -->
-            <?php
-                include ("src/twitter_timeline.php");
-                ?>
+<! -- build up the table row that displays the last manual watering intiation timestamp - if the feature toggle has been set -->
+<?php
+    include ("src/last_manual_watering_timestamp.php");
+    ?>
+<! -- build up the table row that displays the moisture data as a graph -
+if the feature toggle has been set -->
+<?php
+    include ("src/moistchart.php");
+    ?>
 
-    </table>
+
+<! -- build up the table row that displays the twitter timeline for nanismusKW -
+if the feature toggle has been set -->
+<?php
+    include ("src/twitter_timeline.php");
+    ?>
+
+</table>
 
 
 </body>
