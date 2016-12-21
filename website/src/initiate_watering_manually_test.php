e16db386671bffe0576bf7eb43dea840d1e203ee
Stefan <stefanwilluda@macbook-air.local>
2016-12-21 19:14:12 +0100

    related to #92

---
+++ website/src/initiate_watering_manually_test.php
@@ -0,0 +1,70 @@
+<?php
+    
+    // Idee von
+    // Autor: Jens Leopold
+    //
+    // Kontakt: info@jleopold.de
+    //
+    //
+    // P r o j e k t: Arduino-Aquarium-Temperatur-Überwachung
+    // -------------------------------------------------------
+    //
+    // PHP-Skript, welches von Arduino benutzt wird, um Werte in die mySQL-Datenbank einzutragen
+    //
+    // werden keine Daten per "?TEMP=......" ¸bergeben, wird der aktuellste Wert aus der Datenbank angezeigt
+    //
+    
+    // Festlegen welche Datenbank verwendet werden soll
+    $test = false;  // bei true werden die Daten in die Testdatenbank geschrieben
+    
+    $tabelle = "initiate_watering_events";
+    
+    
+    //Datenbank-Verbindung herstellen
+    //--------------------------------
+    include("db.php");
+    
+    if ($botname == "bot"){
+        
+        // pump initiation by user
+        $valueString = "bot-initiat";
+        
+    }
+    else if ($botname == "human"){
+        
+        // pump initiation by user
+        $valueString = "initiat";
+        
+    }
+    
+    // we receive the name from the calling page
+    // $name = "Banane";
+    
+    $sql = "
+        INSERT INTO $tabelle
+        (
+         name , watering_initiated, sender
+         )
+        VALUES
+        (
+        '$name', '$valueString', '$botname'
+         )
+    ";
+    
+    //http://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol
+    // $db_erg = mysqli_query($db_link, $sql)
+    // or die("transmission failure" . mysqli_error($db_link));
+    
+    $db_erg = mysqli_query( $db_link, $sql );
+    if ( ! $db_erg )
+    {
+        die('invalid request: ' . mysqli_error($db_link));
+        
+    }
+    else {
+        
+        // If we performed a successful data transmission that will initiate a watering event, we want to display a nice green background. To do so we simply manipulate the moisture value to 100%
+        $Feuchte = 100;
+    }
+    
+    ?>
